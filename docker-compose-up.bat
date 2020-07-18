485c06a4ca94cfc0182210a360d9b217da1428b8
tornaia <tornai.andras.ede@gmail.com>
2020-07-18 14:30:18 +0200

    Add docker batch file

---
+++ docker-compose-up.bat
@@ -0,0 +1,4 @@
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
+FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi %%i
+docker-compose down
+docker-compose up --build
