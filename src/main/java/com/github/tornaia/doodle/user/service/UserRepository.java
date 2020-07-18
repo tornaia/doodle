fb93c70e9ddc91c9c19170383edbf20fae71109b
tornaia <tornai.andras.ede@gmail.com>
2020-07-18 14:59:31 +0200

    Add User entity, repository and db init script

--- src/main/java/com/github/tornaia/doodle/user/service/UserRepository.java
+++ src/main/java/com/github/tornaia/doodle/user/service/UserRepository.java
@@ -3,5 +3,5 @@ package com.github.tornaia.doodle.user.service;
 import com.github.tornaia.doodle.user.service.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
